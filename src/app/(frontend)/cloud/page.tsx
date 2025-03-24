2c019329578e33f980cf8b53955c9a868d64213c
Christian May <christianmay21@gmail.com>
2025-03-24 16:38:32 -0500

    Added back conditional render

--- src/app/(frontend)/cloud/page.tsx
+++ src/app/(frontend)/cloud/page.tsx
@@ -1,18 +1,32 @@
 'use client'
 
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import WordCloud from '@/app/components/WordCloud'
 import Submit from '@/app/components/Submit'
 
 export default function CloudPage() {
   // Add a state variable that will change to trigger re-renders
   const [refreshKey, setRefreshKey] = useState(0)
-
+  const [showWordCloud, setShowWordCloud] = useState(false)
+  
+  useEffect(() => {
+    // Check if user has already submitted
+    const hasSubmittedCookie = document.cookie
+      .split('; ')
+      .find((row) => row.startsWith('HasAlreadySubmitted='))
+    
+    if (hasSubmittedCookie) {
+      setShowWordCloud(true)
+    }
+  }, [])
+  
   // Function to force re-render of WordCloud
   const refreshWordCloud = () => {
     // Incrementing the state will cause a re-render
     setRefreshKey((prevKey: number) => prevKey + 1)
+    setShowWordCloud(true)
   }
+  
   return (
     <div
       style={{
@@ -22,7 +36,7 @@ export default function CloudPage() {
         marginBottom: 'auto',
       }}
     >
-      <WordCloud key={refreshKey} />
+      {showWordCloud && <WordCloud key={refreshKey} />}
       <Submit onSubmit={refreshWordCloud} />
     </div>
   )
