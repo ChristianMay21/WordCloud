21bfe96b2820516cc62000796370b751503356f2
Christian May <christianmay21@gmail.com>
2025-03-24 16:51:33 -0500

    Added the image

--- src/app/(frontend)/cloud/page.tsx
+++ src/app/(frontend)/cloud/page.tsx
@@ -8,25 +8,25 @@ export default function CloudPage() {
   // Add a state variable that will change to trigger re-renders
   const [refreshKey, setRefreshKey] = useState(0)
   const [showWordCloud, setShowWordCloud] = useState(false)
-  
+
   useEffect(() => {
     // Check if user has already submitted
     const hasSubmittedCookie = document.cookie
       .split('; ')
       .find((row) => row.startsWith('HasAlreadySubmitted='))
-    
+
     if (hasSubmittedCookie) {
       setShowWordCloud(true)
     }
   }, [])
-  
+
   // Function to force re-render of WordCloud
   const refreshWordCloud = () => {
     // Incrementing the state will cause a re-render
     setRefreshKey((prevKey: number) => prevKey + 1)
     setShowWordCloud(true)
   }
-  
+
   return (
     <div
       style={{
@@ -36,6 +36,11 @@ export default function CloudPage() {
         marginBottom: 'auto',
       }}
     >
+      <img
+        src="/art.webp"
+        alt="A young Black woman with shoulder-length braids wearing a medical mask. She stares straight at you with a neutral expression."
+        style={{ width: '300px' }}
+      />
       {showWordCloud && <WordCloud key={refreshKey} />}
       <Submit onSubmit={refreshWordCloud} />
     </div>
