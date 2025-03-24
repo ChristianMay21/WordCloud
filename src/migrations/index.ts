631acba4929daceb6670d0530f0d2333ff430b6a
Christian May <christianmay21@gmail.com>
2025-03-24 14:45:34 -0500

    Added SQL migration

---
+++ src/migrations/index.ts
@@ -0,0 +1,9 @@
+import * as migration_20250324_194449 from './20250324_194449';
+
+export const migrations = [
+  {
+    up: migration_20250324_194449.up,
+    down: migration_20250324_194449.down,
+    name: '20250324_194449'
+  },
+];
